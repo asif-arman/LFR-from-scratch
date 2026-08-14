b50563537ebd49fb5885cf8c4a8326a9e5e8ed0f
Asif Arman <asif2005arman@gmail.com>
2026-08-14 14:09:29 +0600

    Create stage-01-motor-test.ino

---
+++ stage-01-motor-test/stage-01-motor-test.ino
@@ -0,0 +1,24 @@
+constexpr uint8_t ENA = 10;
+constexpr uint8_t ENB = 11;
+constexpr uint8_t IN1 = 6;
+constexpr uint8_t IN2 = 7;
+constexpr uint8_t IN3 = 8;
+constexpr uint8_t IN4 = 9;
+
+void moveLFR(leftMotor,rightMotor){
+  
+}
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(ENA, uint8_t OUTPUT); 
+  pinMode(ENB, uint8_t OUTPUT);
+  pinMode(IN1, uint8_t OUTPUT);
+  pinMode(IN2, uint8_t OUTPUT);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
